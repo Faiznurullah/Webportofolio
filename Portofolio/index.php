35b7564e0643d4d3d2dca3b2a476ffe3a536e5d9
Faiz nurullah <59213454+Richard-Ganteng@users.noreply.github.com>
2020-07-04 07:24:45 +0700

    Add files via upload

---
+++ Portofolio/index.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!--ini favicon-->
+   <link rel="shortcut icon" href="img/favicon.png">
+<!-- Font Awesome CSS-->
+<link rel="stylesheet" href="svgs/all.css" type="css/text">
+
+    <title>Richard(Faiz)</title>
+  </head>
+  <style>
+  .let {
+  color: var(--blue);
+  font-family: sans-serif;
+  }
+  .baris {
+  padding: 5px 5px 5px 5px;
+
+  }
+  .baris img{
+  margin-left: 20px;
+
+  }
+
+  </style>
+  <body>
+
+    <!--navbar-->
+	<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+     <center><h2 class="text-white mt-2">Selamat Datang </h2><img src="img/programmer.png" style="width: 50px; height; 50px;"></center>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+  </nav>
+</div>
+<!--akhir navbar-->
+
+
+
+<!--ini jumbotron-->
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+   <div class="row">
+
+     <div class="col-sm-4">
+  <center> <img src="img/richard.jpg" style="width: 180px; height: 180px;"class="rounded-circle" >
+   <p class="mt-2">&nbsp;"Junior Front Developer"</p></center>
+      </div>
+
+	  <div class="col-sm-8">
+	  <div class="card">
+	   <div class="card-body">
+	   <center><h2>Richard Gans </h2></center>
+     <p><h5>Nama: Faiz Nurullah </h5></p>
+     <p><h5>TTL: Cirebon,22 Maret 2004 </h5></p>
+	   <p>"Hai Teman! Perkenalkan nama saya Richard dengan nama asli Faiz Nurullah Umur saya Baru 16 tahun saya berasal dari kota cirebon. Saya seorang junior front end developer,
+	    Pertamakali saya mengenal Dunia IT di umur 15 Tahun saya belajar otodidak dan saya cukup berkembang akhir-akhir ini"</p>
+
+		<p><center class="let">LET'S JOIN</center></p>
+		<center><p class="baris">
+		<a href="https://m.facebook.com/steven.richard.12"><img src="img/facebook.png" style="width: 50px; height: 50px;" ></a>
+		<a href="https://www.instagram.com/faiz_nurullah/"><img src="img/instagram.png" style="width: 50px; height: 50px;" ></a>
+		<a href="https://api.whatsapp.com/send?phone=62881023954733&text=salken%20bang&source=&data=&app_absent="><img src="img/whatsapp.png" style="width: 50px; height: 50px;"></a>
+		<a href="https://www.google.co.id/maps/dir//Gebang,+Kec.+Gebang,+Cirebon,+Jawa+Barat+45191/@-6.8330268,108.7186763,15.5z/data=!4m8!4m7!1m0!1m5!1m1!1s0x2e6f069a64d7655f:0xf416b2bdaee2d2c5!2m2!1d108.7235887!2d-6.8304459?hl=id" ><img src="img/maps.png" style="width: 50px; height: 50px;"></a>
+		</p></center>
+
+	     </div>
+	   </div>
+	  </div>
+   </div>
+  </div>
+</div>
+
+
+<!--akhir jumbotron-->
+
+
+<!--card maha karya-->
+<center><h2 class="mb-4">Junior Developer</h2></center>
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="img/1.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">web corona</h5>
+      <p class="card-text">Ini adalah karya pertama saya di dunia web programming dimana web digunakan untuk melihat jumlah data yang berkaitan dengan corona atau covid-19.</p>
+      <p class="card-text"><small class="text-muted">Made 2 month ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">E-Izin</h5>
+      <p class="card-text">E-Izin adalah sebuah web berbasis php native di padukan dengan framework bootstrap dan menggunakan mysql sebagai pengatur data.</p>
+      <p class="card-text"><small class="text-muted">Made 1 month ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/3.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Forum IT</h5>
+      <p class="card-text">Membuat simple Forum IT menggunakan php dan mysql dimana member dapat menggunakan tools yang sudah disediakan </p>
+      <p class="card-text"><small class="text-muted">Made 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+
+
+<center><h2 class="mb-4 mt-4">Construct2 Developer</h2></center>
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="img/bubble.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Bubble Game</h5>
+      <p class="card-text">Game Ini adalah karya pertama saya dalam bermain dan sebagai developer construct2 dimana game ini berbasis Html5 dan Javascript</p>
+      <p class="card-text"><small class="text-muted">Made 2 month ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/paddle.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Paddle Ball</h5>
+      <p class="card-text">Game ini Karya kedua saya dalam dunia developer game dimana asset game ini gratis dapat dari google </p>
+      <p class="card-text"><small class="text-muted">Made 1 month ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/pesawat.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Pesawat Angkasa</h5>
+      <p class="card-text">Game karya Ketiga saya dalam dunia developer saya terinspirasi dari game zaman dahulu dimana game ini sudah sangat melegenda waktu saya masih kecil</p>
+      <p class="card-text"><small class="text-muted">Made 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+
+<!--akhir mahakarya-->
+
+
+
+
+<!--skill-->
+
+<center><h2 class="mt-4 mb-4">Keahlian</h2></center>
+
+<div class="progress">
+  <b>HTML</b><div class="progress-bar bg-danger" role="progressbar" style="width: 96%" aria-valuenow="96" aria-valuemin="" aria-valuemax="100">96%</div>
+</div>
+
+<div class="progress mt-3">
+  &nbsp;&nbsp;&nbsp;&nbsp<b>CSS</b><div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+</div>
+
+<div class="progress mt-3">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>JS</b><div class="progress-bar bg-success" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+</div>
+
+<div class="progress mt-3 mb-3">
+  &nbsp;&nbsp;&nbsp;&nbsp<b>PHP</b><div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
+</div>
+<!--akhir skill-->
+
+
+
+<!--Kepribadian-->
+<center><h2 class="mt-4 mb-4">Tentang Saya</h2></center>
+<div class="alert alert-success" role="alert">
+ <p>"Hai Teman! saya Richard(faiz nurullah) aku seorang pelajar Sma namun saya sangat mencintai dunia IT dan berkecinambungan di dalamnya saya sekolah di Sman 1 Babakan sekarang saya mnginjak kelas 11
+ dan berumur 16 Tahun. Saya Lahir Di cirebon pada 22,maret,2004 dan menetap disini sampai sekarang saya adalah anak semata wayang alias satu-satunya yang dimiliki orang tua saya. Sebagai seorang pemuda dan pelajar
+ seharusnya kita isi masa muda kita melalui berkarya karena pemuda merupakan bagai tunas untuk negeri ini ingat negeri ini tidak bakal maju jika pemudanya sangat malas, saya suka dunia IT khususnya web developer sejak umur 15 tahun,
+ saya belajar menjadi web developer secara otodidak sejak 1 tahun lalu dan saya merasa bahwa ternyata dunia IT itu sangat menyenangkan jika kita menekuninya dan mempelajarinya dengan teratur ingan teknologi itu fun(Fun technology).
+ "</p>
+</div>
+
+
+<!-- akhir kepribadian-- >
+
+
+
+
+
+
+<!--akhir content + alamat-->
+<div class="jumbotron jumbotron-fluid bg-dark">
+  <div class="container">
+   <div class="row">
+    <div class="col-sm-6">
+	<h2><font color="White">ALAMAT:</font></h2>
+	<p class="text-white"><svg class="bi bi-phone" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+  <path fill-rule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+</svg>&nbsp;&nbsp;&nbsp;Phone Number: (+62)881023954733</p>
+    <p class="text-white"><svg class="bi bi-envelope" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
+  <path d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z"/>
+</svg>&nbsp;&nbsp;&nbsp;Email: Faizn103a@gmail.com</p>
+<p class="text-white">Ds.Gebang-Kec.Gebang-Kab.Cirebon </p>
+	<p class="text-white">Jln.Pangeran Sutajaya no:128  &nbsp;&nbsp;kodepos: 45191</p>
+
+
+
+     </div>
+   </div>
+  </div>
+</div>
+
+<!--akhir segala content
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
